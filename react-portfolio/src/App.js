5fd33adc8060055a5cca1381e4bfa79f97e0d14d
Megan McHugh <megan.mchugh@gmail.com>
2022-12-02 14:07:27 -0600

    updated app.js, page, and navbar

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Page from "./components/Page";
 
 function App() {
+  const [Pages] = useState ([
+    {
+      name = "Resume"
+    },
+    {
+      name = "About Me"
+    },
+    {
+      name = "Portfolio"
+    }
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ react-portfolio/src/components/Page/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PageContent from "../Page Content";
+import About from "../About";
+import Portfolio from "../Portfolio";
+import Resume from "../Resume";
+import Contact from "../Contact";
+
+
+export default Page;
