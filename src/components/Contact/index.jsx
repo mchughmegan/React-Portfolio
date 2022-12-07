8b63302f37029b52503a5d40043d112ebc9ac814
Megan McHugh <megan.mchugh@gmail.com>
2022-12-07 09:41:17 -0600

    updated some components and package json

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,9 +2,13 @@ import React from "react";
 function Contact() {
   return (
     <div className="text-center">
-      <a href="https://docs.google.com/forms/d/e/1FAIpQLScL_wd78-OYjCi0SkFAnXSbA01FPDPVHtRj4y0_sAC5ISqcIw/viewform?usp=pp_url&entry.710989998=Full+Name&entry.389136940=email@email.com">
-        Contact Me
-      </a>
+      <div className="rightContainer col-6">
+        <h2>Contact Me</h2>
+        <p>If you need any additional information, please click<a href="https://docs.google.com/forms/d/e/1FAIpQLScL_wd78-OYjCi0SkFAnXSbA01FPDPVHtRj4y0_sAC5ISqcIw/viewform?usp=pp_url&entry.710989998=Full+Name&entry.389136940=email@email.com&entry.552445963=Info+regarding+contact">
+        here
+      </a></p>
+      </div>
+      
     </div>
   );
 }
